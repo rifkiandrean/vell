5910c7f3b5358df5cad89b5e5a993866f2b32609
rifkiandrean <rifkiandrean@gmail.com>
2025-10-11 22:41:26 +0700

    Delete src directory

--- src/app/ers/cafe/admin/upd-moderasi/page.tsx
+++
@@ -1,1 +0,0 @@
-// This file can be deleted. The moderation page has been moved to /upd/hani/moderasi
--- src/lib/google-drive.ts
+++
@@ -1,3 +0,0 @@
-// This file is no longer needed as we are moving away from Google Drive links.
-// It is kept for potential future reference but can be deleted.
-export {};
